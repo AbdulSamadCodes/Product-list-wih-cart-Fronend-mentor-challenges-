d68f1fbb051bca737a730f165ad7898bdae3639f
Abdul Samad <samadcom797@gmail.com>
2024-08-09 00:32:04 +0000

    Final commit

--- src/Confirm order/start_new_order.js
+++ src/Confirm order/start_new_order.js
@@ -1,19 +1,23 @@
 //funcion to start the new order and udpate the UI
 function startNewOrder() {
-  productsArray.splice(0,productsArray.length);
+  productsArray.splice(0, productsArray.length);
   validateEmptyCart(productsArray);
-  
-  document.querySelectorAll(".active-cart__btn").forEach(activeCartBtn => activeCartBtn.remove());
-  Array.from(document.querySelector(".product-cart").children).slice(2).forEach(cartItem => cartItem.remove());
+
+  document
+    .querySelectorAll(".active-cart__btn")
+    .forEach((activeCartBtn) => activeCartBtn.remove());
+  Array.from(document.querySelector(".product-cart").children)
+    .slice(2)
+    .forEach((cartItem) => cartItem.remove());
 }
 
 //function to handle the start new order button
 function handleStartNewOrderBtn() {
   const startNewBtn = document.querySelector(".start-new-order-btn");
 
-  startNewBtn.addEventListener("click",function() {
-      closeDialogBox();
-      startNewOrder();
+  startNewBtn.addEventListener("click", function () {
+    closeDialogBox();
+    startNewOrder();
+    updatCartCounter(document.querySelectorAll(".active-cart__btn"));
   });
 }
-
